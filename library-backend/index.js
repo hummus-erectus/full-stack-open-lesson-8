3f2f4045ef8540db2addcddc125c2f4d7fc0cdb6
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-05-09 11:16:06 +0900

    Complete 8.7
    Implement mutation editAuthor, which can be used to set a birth year for an author.

--- library-backend/index.js
+++ library-backend/index.js
@@ -117,6 +117,10 @@ type Query {
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -148,6 +152,16 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(p => p.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(p => p.name === args.name ? updatedAuthor : p)
+      return updatedAuthor
     }
   }
 }
